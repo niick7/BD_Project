d339864bf7bdf1746da7c0060e71e0e1f5b72c76
Nhan Vo <niick7@gmail.com>
2020-12-09 17:13:36 -0600

    Refactor code

---
+++ src/part1/b/WordCount.java
@@ -0,0 +1,79 @@
+package part1.b;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+  public static class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private Map<String, Integer> map = new LinkedHashMap<>();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        String word = itr.nextToken();
+        if(map.containsKey(word)) {
+          int cnt = (int)map.get(word) + 1;
+          map.put(word, cnt);
+        }
+        else map.put(word, 1);
+      }
+    }
+
+    @Override
+    public void cleanup(Context context) throws IOException, InterruptedException {
+      super.cleanup(context);
+      Iterator<Map.Entry<String, Integer>> temp = map.entrySet().iterator();
+
+      while(temp.hasNext()) {
+        Map.Entry<String, Integer> entry = temp.next();
+        String keyVal 	= entry.getKey();
+        Integer countVal= entry.getValue();
+
+        context.write(new Text(keyVal), new IntWritable(countVal));
+      }
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      context.write(key, new IntWritable(sum));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    Job job = new Job(conf, "InMapperWordCount");
+    job.setJarByClass(WordCount.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setReducerClass(Reduce.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.waitForCompletion(true);
+  }
+}
---
+++ src/part1/d/IntPairWritable.java
@@ -0,0 +1,67 @@
+package part1.d;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Objects;
+
+public class IntPairWritable implements WritableComparable<IntPairWritable> {
+  private int first = 0;
+  private int second = 0;
+
+  public IntPairWritable() {}
+
+  public IntPairWritable(int left, int right) {
+    first = left;
+    second = right;
+  }
+
+  public void set(int left, int right) {
+    first = left;
+    second = right;
+  }
+
+  public int getFirst() {
+    return first;
+  }
+
+  public int getSecond() {
+    return second;
+  }
+
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    first = in.readInt();
+    second = in.readInt();
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+    out.writeInt(first);
+    out.writeInt(second);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(first, second);
+  }
+
+  @Override
+  public boolean equals(Object right) {
+    if (right instanceof IntPairWritable) {
+      IntPairWritable r = (IntPairWritable) right;
+      return r.first == first && r.second == second;
+    } else return false;
+  }
+
+  @Override
+  public int compareTo(IntPairWritable tp) {
+    int cmp = (first - tp.first);
+    if (cmp != 0) {
+      return cmp;
+    }
+    return (second - tp.second);
+  }
+}
